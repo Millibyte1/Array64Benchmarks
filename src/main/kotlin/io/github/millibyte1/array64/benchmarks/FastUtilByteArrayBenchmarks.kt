97680d3385fbac148a91e3c3d55b24690b032900
Evan Hall <ebhall99@gmail.com>
2021-06-02 16:01:39 -0400

    Initial commit

---
+++ src/main/kotlin/io/github/millibyte1/array64/benchmarks/FastUtilByteArrayBenchmarks.kt
@@ -0,0 +1,19 @@
+package io.github.millibyte1.array64.benchmarks
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.OutputTimeUnit
+import java.util.concurrent.TimeUnit
+
+class FastUtilByteArrayBenchmarks {
+
+    @Benchmark @BenchmarkMode(Mode.All) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    fun sequentialAccess() {
+
+    }
+    @Benchmark @BenchmarkMode(Mode.All) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    fun randomAccess() {
+
+    }
+}
---
+++ src/main/kotlin/io/github/millibyte1/array64/benchmarks/FastUtilObjectArrayBenchmarks.kt
@@ -0,0 +1,23 @@
+package io.github.millibyte1.array64.benchmarks
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.OutputTimeUnit
+import java.util.concurrent.TimeUnit
+
+class FastUtilObjectArrayBenchmarks {
+
+    @Benchmark @BenchmarkMode(Mode.All) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    fun sequentialAccessViaForEach() {
+
+    }
+    @Benchmark @BenchmarkMode(Mode.All) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    fun sequentialAccessViaIterator() {
+
+    }
+    @Benchmark @BenchmarkMode(Mode.All) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    fun randomAccessViaSubscript() {
+
+    }
+}
